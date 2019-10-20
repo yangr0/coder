4dc29db07926c6829a0f9e7c6d599d5644c6127d
inc0gnit0 <iinc0gnit0@pm.me>
2019-10-19 21:40:32 -0500

    Update coder.py

--- coder.py
+++ coder.py
@@ -149,7 +149,7 @@ def main():
 	print(random.choice(list) + "                                     Email: iinc0gnit0@pm.me")
 	time.sleep(0.1)
 	print("\n")
-	print(random.choice(list) + "                                     Instagram: i.nc0gnit0")
+	print(random.choice(list) + "                                     Instagram: inc0gnit0.offical")
 	time.sleep(0.1)
 	print("\n\033[0m")
 
